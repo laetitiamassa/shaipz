0e199fe133b85e13a9e249d2bad309de4822fc94
Romain Gérard <romaingweb@gmail.com>
2012-06-26 14:13:24 +0200

    Create application skeleton with Cucumber and Rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shaipz::Application.config.secret_token = '115a96e3147beb902359cbe3f91b5e0772aa9bd7d4d18b5bb5940c5c12a7b55ef4acd489f0e456a47be1d4ffce601e6b116060a11d199917df5edc38bca36d73'
