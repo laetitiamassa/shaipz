6f48b55b786a05ec7e22c1308640f5e4078a53c7
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2012-07-09 11:04:39 +0200

    Premier commit

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -4,6 +4,7 @@ class ParticipationsController < ApplicationController
     @participation.participant = current_user
     if @participation.save
       flash[:notice] = t("participation.create_success")
+      NotificationMailer.new_participant(current_user, @participation.project).deliver
       redirect_to stream_path
     else
       flash[:alert] = t("participation.create_error")
