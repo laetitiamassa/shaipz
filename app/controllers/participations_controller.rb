ea270471163b8931d14d2ff3b419dc860bae4d9a
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2012-07-11 20:17:24 +0200

    Merging reports

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -5,10 +5,10 @@ class ParticipationsController < ApplicationController
     if @participation.save
       flash[:notice] = t("participation.create_success")
       NotificationMailer.new_participant(current_user, @participation.project).deliver
-      redirect_to stream_path
+      redirect_to @participation.project
     else
       flash[:alert] = t("participation.create_error")
-      redirect_to stream_path
+      redirect_to @participation.project
     end
   end
 end
