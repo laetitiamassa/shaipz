a83d12cc4579b019f0b4669e29e13790451c61b6
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2012-07-17 13:20:41 +0200

    leaving_project bug

--- app/models/project.rb
+++ app/models/project.rb
@@ -68,7 +68,7 @@ class Project < ActiveRecord::Base
       me = FbGraph::User.me(session[:fb_access_token])
       me.feed!(
         :message => message,
-        :picture => "http://" + request.host + self..picture.url,
+        :picture => "http://" + request.host + self.picture.url,
         :link => url,
         :name => self.name,
         :description =>  status
