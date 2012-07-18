2762318729a1bf9f0018c219cdb331797b33575b
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2012-07-18 12:29:49 +0200

    facebook like @ twitter

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -208,7 +208,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   require "omniauth-facebook"
   config.omniauth :facebook, "268091083289955", "2ec6b4967f5df847b524fcb9026d9529",
-        {:scope => 'email, offline_access, publish_stream', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}, :display => 'popup'}
+        {:scope => 'email, offline_access, publish_stream', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
