de4d125266fd0bfb06f3dabbc39fd893bac80f91
Romain Gérard <romaingweb@gmail.com>
2012-07-23 15:13:07 +0200

    Change config for mailer default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Shaipz::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'shaipz.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'shaipz.com' }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
