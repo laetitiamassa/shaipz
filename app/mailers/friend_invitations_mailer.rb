08c12cdcd4904318b368db5854997a94b08015cd
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2012-07-28 15:22:06 +0200

    Spread the word

--- app/mailers/friend_invitations_mailer.rb
+++ app/mailers/friend_invitations_mailer.rb
@@ -1,10 +1,10 @@
 class FriendInvitationsMailer < ActionMailer::Base
-  default from: "no-reply@shaipz.com"
+  default from: "shaipz@shaipz.com"
 
   def new(user,emails,message) #when a participant is joining
     @user    = user
     @emails = emails
-    @message = message.gsub(/\n/, '<br />')
+    @message = message.gsub(/\n/, '</br>')
     mail(
       :to =>  @emails,
       :cc => @user.email,
