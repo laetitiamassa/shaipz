1088549182305a329f01f319b84322e29bdddd29
Romain Gérard <romaingweb@gmail.com>
2012-07-30 12:26:40 +0200

    Little improvement in friend mailer

--- app/mailers/friend_invitations_mailer.rb
+++ app/mailers/friend_invitations_mailer.rb
@@ -1,13 +1,11 @@
 class FriendInvitationsMailer < ActionMailer::Base
-  default from: "Shaipz <no-reply@shaipz.com>"
+  default from: "Shaipz <hello@shaipz.com>"
 
-  def new(user,emails,message) #when a participant is joining
+  def new(user, emails, message)
     @user    = user
     @emails = emails
     @message = message
-    mail(
-      :to =>  @emails,
-      :subject => t("friend_invitation.mailer.subject", :user => @user.name)
-    )
+    mail(:to =>  @emails,
+         :subject => t("friend_invitation.mailer.subject", :user => @user.name))
   end
 end
