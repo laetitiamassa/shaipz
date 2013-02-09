3b70be47e302c0c7b4432cd7d022b5d9d6be1635
Laetitia Massa <laetitiamassa@gmail.com>
2013-02-09 09:50:44 +0100

    correction personal timing
    step by step correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
       @urls = @url_immo.generate_urls
       @building_types = @url_immo.building_types
       @personal_statuses = User.personal_statuses
+      @personal_timing = User.personal_timing
       flash[:alert] = t("profile.update_errors")
       render :edit
     end
