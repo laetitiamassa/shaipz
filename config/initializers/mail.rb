738fbb20c87cb02e1bee93420b5ef43788c17538
Laetitia Massa <laetitiamassa@gmail.com>
2013-05-08 07:24:17 +0200

    try to modify elements to use mailjet

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -4,6 +4,6 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['8473030f2f6a78e70a830db1d29c8397'],
   :password       => ENV['b2303689584e4ef961cc9393b90bf726'],
-  :domain         => 'shaipz.com'
+  :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
