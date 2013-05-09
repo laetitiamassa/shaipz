e1b81c45d7848b3de5623b94d5c49c2c0ba628fb
Laetitia Massa <laetitiamassa@gmail.com>
2013-05-09 10:23:38 +0200

    corrected mailjet settings

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
+  :address        => 'in.mailjet.com',
   :port           => '587',
+  :enable_starttls_auto => true,
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => '8473030f2f6a78e70a830db1d29c8397',
+  :password       => 'b2303689584e4ef961cc9393b90bf726',
   :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
