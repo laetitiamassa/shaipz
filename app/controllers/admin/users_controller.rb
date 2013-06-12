2aec1a87fb4b94a1b478f36b862d31f83e40491f
Laetitia Massa <laetitiamassa@gmail.com>
2013-06-13 00:25:28 +0200

    adapted user_buying stats in admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < Admin::BaseController
   def index
     @users = User.order('created_at DESC')
     @user_active = User.where("current_sign_in_at < ?", 31.days.ago)
-    @user_buying = User.where("personal_status" == "buying")
+    @user_buying = User.where(:personal_status => "buying")
     respond_to do |format|
       format.html
       format.csv { send_data @users.to_csv }
