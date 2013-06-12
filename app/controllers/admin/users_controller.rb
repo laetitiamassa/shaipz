12845889b320f54fc2347b92af04b2d53677ff81
Laetitia Massa <laetitiamassa@gmail.com>
2013-06-13 00:44:02 +0200

    added stats to user admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,9 @@ class Admin::UsersController < Admin::BaseController
     @users = User.order('created_at DESC')
     @user_active = User.where("current_sign_in_at < ?", 31.days.ago)
     @user_buying = User.where(:personal_status => "buying")
+    @user_ready_but_bank = User.where(:personal_status => "ready_but_bank")
+    @user_ready_with_bank = User.where(:personal_status => "ready_with_bank")
+    @user_hiding_budget = User.where(:hide_budget => true)
     respond_to do |format|
       format.html
       format.csv { send_data @users.to_csv }
