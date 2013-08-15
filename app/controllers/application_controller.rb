e17d077926d216ee3e4fefc5b4837ac0ac82bd29
laetitia massa <laetitiamassa@gmail.com>
2013-08-15 15:27:14 +0200

    testing put methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,32 @@ class ApplicationController < ActionController::Base
   comment_destroy_conditions do |comment|
     comment.owner == current_user
   end
+
+  def fellows_in_district
+    User.all.select do |user|
+      user.id != self.id &&
+      (user.zipcodes & self.zipcodes).any?
+    end
+  end
+
+  def fellows_status
+    User.where("users.id != :id AND users.personal_status = :personal_status", { id: self.id, personal_status: self.personal_status })
+  end
+
+  def fellows_purpose
+    User.where("users.id != :id AND users.rationale = :rationale", { id: self.id, rationale: self.rationale } )
+  end
+
+  def fellows
+    fellows_in_district | fellows_status | fellows_purpose
+  end
+
+  def perfect_fellows
+    fellows_in_district && fellows_status && fellows_purpose
+  end
+
+  def zipcodes
+    favorite_areas.gsub(" ", "").split(",")
+  end
+  
 end
